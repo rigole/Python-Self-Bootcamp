d9e0f71779d5d92b61b5443ecff6cddbbd366343
rigole <foplacide@gmail.com>
2022-06-17 14:39:07 +0100

    first commit

---
+++ tutrle-crossing/car_manager.py
@@ -0,0 +1,15 @@
+from turtle import Turtle
+import random
+
+COLORS=["red", "orange", "yellow", "green", "blue", "purple"]
+STARTING_MOVE_DISTANCE = 5
+MOVE_INCREMENT = 10
+
+class CarManager:
+    
+    def __init__(self):
+        all_cars = []
+        
+        
+    def create_cars(self):
+        new_car = Turtle("square")
---
+++ tutrle-crossing/main.py
@@ -0,0 +1,18 @@
+import time
+from turtle import Screen
+from player import Player
+
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+player = Player()
+
+screen.listen()
+screen.onkey(player.go_up, "Up")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
