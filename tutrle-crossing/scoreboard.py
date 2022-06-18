9393bbefe3ae40ede1040dd2484107cf68d77a6e
rigole <foplacide@gmail.com>
2022-06-18 14:09:19 +0100

    score setting and update

--- tutrle-crossing/scoreboard.py
+++ tutrle-crossing/scoreboard.py
@@ -1,4 +1,25 @@
-FONT =("Courier", 24, "normal")
+from turtle import Turtle
 
-class Scorebord:
-    pass
+
+FONT =  ("Courier", 24, "normal")
+
+class Scorebord(Turtle):
+    
+    def __init__(self):
+        super().__init__()
+        self.level = 1
+        self.hideturtle()
+        self.penup()
+        self.goto(-280, 250)
+        self.update_scoreboard()
+        
+        
+    def update_scoreboard(self):
+        self.write(f"Level: {self.level}", align="left", font=FONT)
+       
+    
+    def increasing_level(self):
+        self.clear()
+        self.level += 1
+        self.update_scoreboard()
+        
