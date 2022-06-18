d8ab6162e2e259b672b15d66d4b90163a37e7e11
rigole <foplacide@gmail.com>
2022-06-18 11:53:51 +0100

    detecting collision

--- tutrle-crossing/main.py
+++ tutrle-crossing/main.py
@@ -21,3 +21,14 @@ while game_is_on:
     
     car_manager.create_cars()
     car_manager.move_cars() 
+    
+    #Detect collision with car
+    
+    
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            
+            
+screen.exitonclick()
+    
