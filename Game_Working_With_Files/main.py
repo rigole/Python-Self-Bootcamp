eb42e1b45a3b14d2a819b262b754d2412f49cc68
rigole <foplacide@gmail.com>
2022-06-21 20:06:20 +0100

    collision with tail

--- Game_Working_With_Files/main.py
+++ Game_Working_With_Files/main.py
@@ -40,6 +40,15 @@ while game_is_on:
     if(snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280):
         game_is_on = False
         scoreboard.game_over()
+        
+        
+    #collision tail
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False 
+            scoreboard.game_over()
     
     
 screen.exitonclick()
