2ccae025f68667f26b7b5489ebcdf8d7868c61f6
rigole <foplacide@gmail.com>
2022-06-22 13:20:56 +0100

    Moving paddle using keypress 2

--- Pong_Game/main.py
+++ Pong_Game/main.py
@@ -5,6 +5,7 @@ screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("Pong")
+screen.tracer(0)
 
 
 paddle = Turtle()
@@ -27,5 +28,9 @@ screen.listen()
 screen.onkey(go_up, "Up")
 screen.onkey(go_down, "Down")
 
+game_is_on = True
+
+while game_is_on:
+    screen.update()
 
 screen.exitonclick()
