221869da5870ef09085e29f36b6ca90737ce7edb
rigole <foplacide@gmail.com>
2022-06-29 09:44:28 +0100

    adding the high score in the snake game

--- Game_Working_With_Files/main.py
+++ Game_Working_With_Files/main.py
@@ -38,8 +38,10 @@ while game_is_on:
         
     
     if(snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280):
-        game_is_on = False
-        scoreboard.game_over()
+        #game_is_on = False
+        #scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
         
         
     #collision tail
@@ -47,8 +49,11 @@ while game_is_on:
         if segment == snake.head:
             pass
         elif snake.head.distance(segment) < 10:
-            game_is_on = False 
-            scoreboard.game_over()
+            #game_is_on = False 
+            #scoreboard.game_over()
+            scoreboard.reset()
+            snake.reset()
+            
     
     
 screen.exitonclick()
--- Game_Working_With_Files/snake.py
+++ Game_Working_With_Files/snake.py
@@ -55,3 +55,11 @@ class Snake(Turtle):
         new_segment.penup()
         new_segment.goto(position)
         self.segments.append(new_segment)
+        
+        
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000, 1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
