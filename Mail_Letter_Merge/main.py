5797eb9d1c2efdc233c4515064501aef6829d400
rigole <foplacide@gmail.com>
2022-07-03 13:35:58 +0100

    Letter merge

--- Mail_Letter_Merge/main.py
+++ Mail_Letter_Merge/main.py
@@ -1,10 +1,17 @@
+from logging import PlaceHolder
 
+
+PLACEHOLDER = "[name]"
 
 with open("./Input/Names/invited_names.txt") as name_file:
     names = name_file.readlines()
-    print(names)
+    #print(names)
     
     
 with open("./Input/Letters/starting_letter.txt") as letter_file:
     letter_contents = letter_file.read()
-    
+    for name in names:
+        stripped_name = name.strip()
+        new_letter = letter_contents.replace(PLACEHOLDER, stripped_name)
+        with open(f"./Output/ReadyToSend/letter_for_{stripped_name}.txt", mode="w") as complete_letter:
+            complete_letter.write(new_letter)
