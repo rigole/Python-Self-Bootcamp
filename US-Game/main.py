ed57b3152b235891482bf295208b1cfd32635149
rigole <foplacide@gmail.com>
2022-07-07 14:33:44 +0100

    getting user input until states finished

--- US-Game/main.py
+++ US-Game/main.py
@@ -13,19 +13,21 @@ turtle.shape(image)
 ##turtle.onscreenclick(get_mouse_click_coor)
 
 data = pandas.read_csv("50_states.csv")
-
 all_states = data.state.to_list()
+guessed_states = []
 
-answer_state = screen.textinput(title="Guess the state", prompt="What's another's state name")
-print(answer_state)
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title="Guess the state", prompt="What's another's state name")
+    print(answer_state)
 
-
-if answer_state in all_states:
-    t = turtle.Turtle()
-    t.hideturtle()
-    t.penup()
-    state_data = data[data.state == answer_state]
-    t.goto(int(state_data.x), int(state_data.y))
-    t.write(state_data.state)
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(state_data.state.item())
+   
     
 screen.exitonclick()
