3e113141589a1ef736346c87259cc4a122070a2f
rigole <foplacide@gmail.com>
2022-07-09 12:06:45 +0100

    saving guessed staes into a new  csv files

--- US-Game/main.py
+++ US-Game/main.py
@@ -17,9 +17,17 @@ all_states = data.state.to_list()
 guessed_states = []
 
 while len(guessed_states) < 50:
-    answer_state = screen.textinput(title="Guess the state", prompt="What's another's state name")
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States  Correct", prompt="What's another's state name")
     print(answer_state)
 
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
     if answer_state in all_states:
         guessed_states.append(answer_state)
         t = turtle.Turtle()
