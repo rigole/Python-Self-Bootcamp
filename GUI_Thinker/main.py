355afd1146a12494ecd89ecaf07bd455fb5bf076
rigole <foplacide@gmail.com>
2022-07-16 20:03:52 +0100

    testing label and button and click command on tkinter library

--- GUI_Thinker/main.py
+++ GUI_Thinker/main.py
@@ -1,3 +1,4 @@
+""" 
 import tkinter
 
 window = tkinter.Tk()
@@ -7,6 +8,37 @@ window.minsize(width=500, height=300)
 
 my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "bold"))
 
-my_label.pack(expand=True)
+my_label.pack(side="right")
 
 window.mainloop()
+
+
+
+def add(*args):
+    total = 0
+    for i in args:
+        
+        total += i
+        
+    return total
+
+print(add(12, 15, 25))
+"""
+
+
+
+
+from tkinter import *
+
+window = Tk()
+
+window.title("My First GUI Program")
+window.minsize(width=500, height=300)
+
+
+my_label = Label(text="Here is a label", font=("Arial",24, "bold"))
+#my_label.pack(side="left")
+
+
+button = Button(text="Click here")
+button.pack()
