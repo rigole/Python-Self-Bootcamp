3d5452fb338b25687723b377a352fee767a837d6
rigole <foplacide@gmail.com>
2022-07-17 13:57:25 +0100

    setting the pistion of element using grid to make them visible to the screen

--- GUI_Thinker/converter.py
+++ GUI_Thinker/converter.py
@@ -4,14 +4,23 @@ window = Tk()
 window.title("Converter to kilometer")
 
 miles_input = Entry()
+miles_input.grid(column=1, row=0)
 
 miles_label = Label(text="Miles")
+miles_label.grid(column=2, row=0)
 
 is_equal_label = Label(text="is equal to")
+is_equal_label.grid(column=0, row=1)
 
 kilometer_result_label = Label(text="0")
+kilometer_result_label.grid(column=1, row=1)
+
 
 kilometer_label = Label(text="Km")
+kilometer_label.grid(column=2, row=1)
+
+calculate_button = Button(text="Calculate")
+calculate_button.grid(column=1, row=2)
 
 
 window.mainloop()
