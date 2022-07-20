d58317dc62116671c492711d90ef3552d379e7c3
rigole <foplacide@gmail.com>
2022-07-20 20:54:14 +0100

    setting for long, short and break

--- Pomodoro-App/main.py
+++ Pomodoro-App/main.py
@@ -1,25 +1,53 @@
 from tkinter import *
 from turtle import title
+import math
 
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
+WORK_MIN = 1
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+repetitions = 0
 
 # Setting the timing
 def start_timer():
-    count_down(5)
+    global repetitions
+    repetitions += 1 
+    
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+    
+    # 1st, 3rd,5th,7th
+    count_down(work_sec)
+    if repetitions % 8 == 0:
+        count_down(long_break_sec)
+    elif repetitions % 2 == 0:
+        count_down(short_break_sec)
+    else:
+        count_down(work_sec)
+    
+    
+    
+    
+    
+    
+    
+    count_down(5 * 60)
 
 
 
 
 def count_down(count):
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
     
-    canvas.itemconfig(timer_text, text=count)
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000, count_down, count - 1)
         
