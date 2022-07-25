c6bf0098a7e6394c0b4c0a0bb02765c299b0cfac
rigole <foplacide@gmail.com>
2022-07-25 15:00:31 +0100

    using messageBox to display message to user 2

--- Password_Manager/main.py
+++ Password_Manager/main.py
@@ -10,14 +10,19 @@ def save():
     email = email_entry.get()
     password = password_entry.get()
     
-    messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} \nPassword: {password} \n Is it okay to save")
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Error", message="Please make sure all fields are not empty")
+    else:
+        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} \nPassword: {password} \n Is it okay to save")
+        if is_ok:
+            with open("data.txt", "a") as data_file:
+                data_file.write(f"| {website} | {email} | {password}\n")
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
+
+
+        
     
-    with open("data.txt", "a") as data_file:
-        data_file.write(f"| {website} | {email} | {password}\n")
-        website_entry.delete(0, END)
-        password_entry.delete(0, END)
-
-
 
 
 
