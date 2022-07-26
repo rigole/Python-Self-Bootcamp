68ee39e661ab80a07c9d84da2410236d1711c42d
rigole <foplacide@gmail.com>
2022-07-26 08:17:22 +0100

    password generate, pyperclipcopy added

--- Password_Manager/main.py
+++ Password_Manager/main.py
@@ -1,18 +1,44 @@
 from tkinter import *
 from tkinter import messagebox
 import random
-
+import pyperclip
 #Password generator
-letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','y','z']
-numbers = ['0','1','2','3','4','5','6','7','8','9']
-symbols = ['!','#','$','&','%','(',')', '*','+']
+# u(#d86mo3le1sjx
+def password_generator():
+    letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','y','z']
+    numbers = ['0','1','2','3','4','5','6','7','8','9']
+    symbols = ['!','#','$','&','%','(',')', '*','+']
 
-nr_letters = random.randint(8, 10)
-nr_symbols = random.randint(2, 4)
-nr_numbers = random.randint(2, 4)
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_letters = [random.choice(letters) for _ in range(nr_letters)]
+    password_symbols = [random.choice(symbols) for _ in range(nr_symbols)]
+    password_numbers = [random.choice(numbers) for _ in range(nr_numbers)]
+    #for char in range(nr_letters):
+    #
+    # password_list.append(random.choice(letters))
+    
+    #for char in range(nr_symbols):
+    #    password_list += random.choice(symbols)
+    
+    #for char in range(nr_numbers):
+    #    password_list += random.choice(numbers)
+    
+    
+    password_list = password_letters + password_symbols + password_numbers
+    random.shuffle(password_list)
+
+    password = "".join(password_list)
+    #print(f"Your password is: {password}")
+    password_entry.insert(0, password)
+
+    #for char in password_list:
+        #password += char
 
 
-#Saving Password
+    #Saving Password
 
 def save():
     
@@ -88,11 +114,11 @@ website_entry.focus()
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=2)
 email_entry.insert(0, "foplacide@gmail.com")
-password_entry = Entry(width=21)
+password_entry = Entry(width=30)
 password_entry.grid(row=3, column=1, columnspan=1)
 
 # Buttons
-generate_password_button = Button(text="Generate Password")
+generate_password_button = Button(text="Generate Password", command=password_generator)
 generate_password_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=35, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
