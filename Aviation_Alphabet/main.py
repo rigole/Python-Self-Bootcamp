2d417a0bf2939ab3d6f53efbe4e1096a021813ff
rigole <foplacide@gmail.com>
2022-08-02 15:26:27 +0100

    handling key Exception 2

--- Aviation_Alphabet/main.py
+++ Aviation_Alphabet/main.py
@@ -3,13 +3,20 @@ import pandas as pd
 # creating a dictionary
 
 data = pd.read_csv("nato_phonetic_alphabet.csv")
-print(data.to_dict())
+#print(data.to_dict())
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
-print(phonetic_dict)
+#print(phonetic_dict)
 
-word = input("Enter a word: ").upper()
+def generate_alphabet():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_alphabet()
+    else:
+        print (output_list)
 
-output_list = [phonetic_dict[letter] for letter in word]
 
-print (output_list)
+generate_alphabet()
