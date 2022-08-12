1d1e524eafab97bac6bd30676a773f94d81e04fb
rigole <foplacide@gmail.com>
2022-08-12 11:21:14 +0100

    setting the changing after 3 seconds

--- Flash-Card/main.py
+++ Flash-Card/main.py
@@ -1,26 +1,34 @@
-from textwrap import fill
+
 from tkinter import *
-import pandas as pd
+import pandas
 import random
 
-
-
-data = pd.read_csv("data/french_words.csv")
+data = pandas.read_csv("french-words.csv")
+#print(data)
 to_learn = data.to_dict(orient="records")
 current_card = {}
 #print(to_learn)
 
 
+     
+     
+
+     
+BACKGROUND_COLOR = "#B1DDC6"
+
+
+
 def next_card():
-    global current_card
-    global flip_timer
+    global current_card,flip_timer
     window.after_cancel(flip_timer)
     current_card = random.choice(to_learn)
-    #print(current_card["French"])
+    #print(current_card["English"])
     canvas.itemconfig(card_title, text="French", fill="black")
     canvas.itemconfig(card_word, text=current_card["French"], fill="black")
     canvas.itemconfig(card_background, image=card_back_img)
-    window.after(3000, func=flip_card)
+    flip_timer = window.after(3000, func=flip_card)
+    
+    
     
 def flip_card():
     canvas.itemconfig(card_title, text="English", fill="white")
@@ -28,26 +36,28 @@ def flip_card():
     canvas.itemconfig(card_background, image=card_back_img)
 
 
-BACKGROUND_COLOR = "#B1DDC6"
+def is_known():
+     to_learn.remove(current_card)
+     print(len(to_learn))
+     data = pandas.DataFrame(to_learn)
+     data.to_csv("data/words_to_learn.csv")
+     next_card()
 
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-
-
-
 flip_timer = window.after(3000, func=flip_card)
 
-
+#spotty waves
 
 canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file="images/card_front.png")
 card_back_img = PhotoImage(file="images/card_back.png")
 
 card_background = canvas.create_image(400, 263, image=card_front_img)
-card_title = canvas.create_text(400, 150, text="Title", font=("Arial", 40, "italic"))
-card_word = canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
+card_title = canvas.create_text(400, 150, text="", font=("Arial", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
 
 
 
