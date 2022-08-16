638ea2ee18852d996b31318d8700a774091838c0
rigole <foplacide@gmail.com>
2022-08-16 19:42:57 +0100

    working with date and time

--- Smtp/main.py
+++ Smtp/main.py
@@ -1,3 +1,4 @@
+""" 
 import smtplib
 from sqlite3 import connect
 
@@ -11,3 +12,14 @@ connexion.starttls()
 connexion.login(user=my_email, password=password)
 connexion.sendmail(from_addr=my_email, to_addrs="foplacide@gmail.com", msg="Subject:\n\n This is the body of the email Hello world")
 connexion.close()
+
+"""
+
+import datetime as dt
+
+now = dt.datetime.now()
+
+year = now.year
+month = now.weekday()
+
+print(month)
