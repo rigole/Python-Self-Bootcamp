a674128a5e06a988bfaf4f1794c7ca9d31b18b79
rigole <foplacide@gmail.com>
2022-08-17 14:26:54 +0100

    sending motivation quotes via email

--- Smtp/main.py
+++ Smtp/main.py
@@ -13,7 +13,7 @@ connexion.login(user=my_email, password=password)
 connexion.sendmail(from_addr=my_email, to_addrs="foplacide@gmail.com", msg="Subject:\n\n This is the body of the email Hello world")
 connexion.close()
 
-"""
+
 
 import datetime as dt
 
@@ -23,3 +23,32 @@ year = now.year
 month = now.weekday()
 
 print(month)
+
+"""
+
+import random
+import smtplib
+import datetime as dt
+
+MY_EMAIL = "stevenswynter@gmail.com"
+MY_PASSWORD = "cybercrea"
+
+
+
+now = dt.datetime.now()
+week = now.weekday()
+
+if week == 2:
+    with open("quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
+        quote = random.choice(all_quotes)
+        
+    print(quote)
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL, 
+            to_addrs=MY_EMAIL, 
+            msg=f"SUBJECT:Monday Motivation\n\n{quote}"
+            )
