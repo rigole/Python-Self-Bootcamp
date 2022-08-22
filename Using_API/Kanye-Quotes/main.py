7594ce6fcd793d589e2cfe12798ca36c967a2069
rigole <foplacide@gmail.com>
2022-08-22 20:37:25 +0100

    project of ISS api setting

--- Using_API/Kanye-Quotes/main.py
+++ Using_API/Kanye-Quotes/main.py
@@ -1,8 +1,13 @@
-from curses import window
 from tkinter import *
-
+import requests
 def get_quote():
-    pass
+    response = requests.get("https://api.kanye.rest")
+    response.raise_for_status()
+    data = response.json()
+    quote = data["quote"]
+    canvas.itemconfig(quote_text, text=quote)
+    
+    
 
 window = Tk()
 window.title("Kanye Says...")
--- Using_API/main.py
+++ Using_API/main.py
@@ -1,14 +1,36 @@
+from time import time
 import requests
+from datetime import datetime
+
+
+MY_LAT = 	7.369722
+MY_LONG = 12.354722
+
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
-
+response.raise_for_status()
 data = response.json()
 
-longitude = data["iss_position"]["longitude"]
 
-latitude = data["iss_position"]["latitude"]
 
-iss_position = (longitude, latitude)
+iss_longitude = float(data["iss_position"]["longitude"])
 
-print(iss_position)
+iss_latitude = float(data["iss_position"]["latitude"])
 
+#iss_position = (longitude, latitude)
+
+#print(iss_position)
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted":0,
+}
+
+reponse = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+data = response.json()
+sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+time_now = datetime.now()
